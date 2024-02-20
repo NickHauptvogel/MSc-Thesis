1986ade6074a581cfa0ecdb2a0a22790335d5d61
Nick Hauptvogel <nick@hauptvogel.cc>
2024-02-20 17:04:46 +0100

    moving

--- sort_out_files.py
+++ sort_out_files.py
@@ -1,18 +1,43 @@
 import os
 
 
-folder = 'CNN-LSTM_IMDB/results/30_independent_wenzel_hyperparams_random_val'
-out_files_folder = 'CNN-LSTM_IMDB'
+def sort_out_files():
 
-# Get all .out files in out_files_folder
-out_files = [f for f in os.listdir(out_files_folder) if f.endswith('.out')]
-# Get all subdirectories in folder
-subdirs = [f.path for f in os.scandir(folder) if f.is_dir()]
+    folder = 'ResNet20_CIFAR/results/epoch_budget_checkp'
+    out_files_folder = 'ResNet20_CIFAR'
 
-for out_file in out_files:
-    # Get the experiment id
-    experiment_id = int(out_file.split('_')[1].replace('.out', ''))
-    # Find the subdirectory with the same experiment id
-    subdir = [f for f in subdirs if f.endswith(f'_{experiment_id:02d}')][0]
-    # Move the .out file to the subdirectory
-    os.rename(os.path.join(out_files_folder, out_file), os.path.join(subdir, out_file))
+    # Get all .out files in out_files_folder
+    out_files = [f for f in os.listdir(out_files_folder) if f.endswith('.out')]
+    # Get all subdirectories in folder
+    subdirs = [f.path for f in os.scandir(folder) if f.is_dir()]
+
+    for out_file in out_files:
+        print(out_file)
+        # Get the experiment id
+        experiment_id = int(out_file.split('_')[-1].replace('.out', ''))
+        # Find the subdirectory with the same experiment id
+        subdir = [f for f in subdirs if f.endswith(f'{experiment_id:02d}')][0]
+        # Move the .out file to the subdirectory
+        os.rename(os.path.join(out_files_folder, out_file), os.path.join(subdir, out_file))
+
+
+def sort_epoch_budget_folders():
+    target_folder = 'ResNet20_CIFAR/results/epoch_budget_checkp'
+    current_folder = 'ResNet20_CIFAR/results'
+
+    # Get all subdirectories in current_folder that start with 2024
+    subdirs = [f.path for f in os.scandir(current_folder) if f.is_dir() and f.name.startswith('2024')]
+
+    for subdir in subdirs:
+        # Get the ensemble size from the subdirectory name
+        ensemble = subdir.split('_')[-2]
+        model = subdir.split('_')[-1]
+        # Make sure ensemble folder exists in target_folder
+        ensemble_folder = os.path.join(target_folder, ensemble)
+        if not os.path.exists(ensemble_folder):
+            os.makedirs(ensemble_folder)
+        # Move the subdirectory to the ensemble folder
+        os.rename(subdir, os.path.join(ensemble_folder, model))
+
+if __name__ == '__main__':
+    sort_out_files()
